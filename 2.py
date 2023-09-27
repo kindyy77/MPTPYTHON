fa2de26fff18e816437ae0e478483504f4a35e1a
kindyy77 <63909055+kindyy77@users.noreply.github.com>
2023-09-27 15:42:10 +0300

    Второе задание

---
+++ 2.py
@@ -0,0 +1,8 @@
+width = 0
+height = 0
+width = int(input("Введите ширину : "))
+height = int(input("Введите высоту : "))
+def rectangle_area(width, height):
+    result = width * height
+    return result
+print("Площадь равна : ", rectangle_area(width, height))
